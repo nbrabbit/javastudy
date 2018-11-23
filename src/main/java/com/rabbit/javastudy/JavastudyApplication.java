9bfd153a8192e0499a7755a4343c5d342cd81712
nbrabbit <nbrabbit@126.com>
2018-11-23 11:42:36 +0800

    Initial commit

---
+++ src/main/java/com/rabbit/javastudy/JavastudyApplication.java
@@ -0,0 +1,12 @@
+package com.rabbit.javastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastudyApplication.class, args);
+    }
+}
